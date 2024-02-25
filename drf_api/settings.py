50cf47ef91d74368c1f167bac45c05680fd60f41
Ry-F3 <rhysfeew@gmail.com>
2024-02-25 15:48:04 +0000

    Correct secret key

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -59,7 +59,7 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-96p*$s8l+=e%*fv_aj4p!ao4qu@w&eml_pgf)ssd1oy=8ka#&j'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
